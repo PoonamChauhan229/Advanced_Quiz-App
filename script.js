1bc161c4d75f4f1707a7a6a507771465e4030be7
Poonam <pooonam.chauhan229@gmail.com>
2022-06-08 00:57:45 +0530

    50 done

--- script.js
+++ script.js
@@ -1,114 +1,101 @@
-var questionList = document.getElementById('questionList')
-var answerList = document.querySelectorAll('.answerList')
-console.log(answerList)
+console.log("hi")
+var quizContainer = document.querySelector('#quizContainer')
+console.log(quizContainer)
+var type, data,quizData;
 
+let card=document.getElementById('card')
+var prevButton=document.getElementById('prevButton')
+var nextButton=document.getElementById('nextButton')
+quizContainer.addEventListener('click', async function(e) {
+    quizContainer.style.display="none";
+    card.style.visibility="visible";
+    prevButton.style.visibility="visible";
+    prevButton.style.visibility="visible";
+    nextButton.style.visibility="visible";
+    type=e.target.id;
+    console.log(type)
+    console.log(`${type}.json`)
+    data = await fetch(`./json/${type}.json`)
+    quizData = await data.json();
+    console.log(quizData)
 
-var a_option = document.getElementById('a_option')
-var b_option = document.getElementById('b_option')
-var c_option = document.getElementById('c_option')
-var d_option = document.getElementById('d_option')
-
-var quizContainer = document.getElementById('quizContainer')
-
-let responseHtml;
-let responseJs;
-let score = 0;
-var htmlQuizData = document.getElementById('htmlQuiz');
-var jsQuizData = document.getElementById('jsQuiz');
-let currentquizCount = 0;
-console.log(currentquizCount)
-
-if (htmlQuizData.addEventListener('click', async function htmlData() {        
-    var dataHtml = await fetch('html.json')
-    responseHtml = await dataHtml.json()
-    console.log(responseHtml)
-    inputData(responseHtml)
-    
-})) { }
-else if (jsQuizData.addEventListener('click', async function jsQuiz() {
-    var dataJs = await fetch('javascript.json')
-    responseJs = await dataJs.json()
-    console.log(responseJs)
-    inputData(responseJs)
-})) { }
-
-
-function getquizData() {
-    deSelect()
-    let currentquizData = quizData[currentquizCount]
-    console.log(currentquizData)
-
-    questionList.innerText = quizData[currentquizCount].question
-    a_option.innerText = quizData[currentquizCount].a;
-    b_option.innerText = quizData[currentquizCount].b;
-    c_option.innerText = quizData[currentquizCount].c;
-    d_option.innerText = quizData[currentquizCount].d;
-
-}
-
-function inputData(response) {
-    
-    console.log(response)
-    console.log(currentquizCount)
-    let currentquizData = response[currentquizCount]
-    console.log(currentquizData)
-    questionList.innerText = response[currentquizCount].question
-    a_option.innerText = response[currentquizCount].a;
-    b_option.innerText = response[currentquizCount].b;
-    c_option.innerText = response[currentquizCount].c;
-    d_option.innerText = response[currentquizCount].d;
-
-    var button = document.getElementById('button')
-    button.addEventListener("click", function submit() {
-        console.log(currentquizCount)
-        let answer = getSelect();
-        console.log(answer)
-        console.log(response)
-        console.log(response[currentquizCount].correct)
-
-
-        if (answer === response[currentquizCount].correct) {
-            score++;
-        }
-        currentquizCount++;
-        console.log(currentquizCount)
-        console.log(response.length)
-        if (currentquizCount <response.length) {
-            inputData(response);
-        }
-        else {
-            quizContainer.innerHTML = `
-                <h1>You have scored ${score}/${response.length}</h1>
-                <button class="btn btn-primary mt-3 mb-3 px-5" type="button" onclick='location.reload()'>Reload</button>
-                `
-        }
-
+   
+    let output=quizData.map((element)=>{
+        return`
+        <div class="card-header card-body" id="questionList">
+                ${element.question}
+        </div>
+            <ul class="list-group list-group-flush" id="optionList">
+                <li class="list-group-item">
+                    <div class="form-check">
+                        <input class="form-check-input answerList" type="radio" name="answer" id="a">
+                        <label class="form-check-label" for="a" id="a_option">
+                        ${element.a}
+                        </label>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="form-check">
+                        <input class="form-check-input answerList" type="radio" name="answer" id="b">
+                        <label class="form-check-label" for="b" id="b_option">
+                        ${element.b}
+                        </label>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="form-check">
+                        <input class="form-check-input answerList" type="radio" name="answer" id="c">
+                        <label class="form-check-label" for="c" id="c_option">
+                        ${element.c}
+                        </label>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="form-check">
+                        <input class="form-check-input answerList" type="radio" name="answer" id="d">
+                        <label class="form-check-label" for="d" id="d_option">
+                        ${element.d}
+                        </label>
+                    </div>
+                </li>
+            </ul> 
+            `
     })
-    // deSelect()
-}
+    var count=0;
+    card.innerHTML=output[0]
+   
+    console.log(output.length)
+    // console.log(output)
+    
+    console.log(nextButton)
 
-
-// console.log(currentquizCount)
-
-function deSelect() {
-    answerList.forEach(item => item.checked = false)
-
-}
-
-function getSelect() {
-    let answer;
-    answerList.forEach(item => {
-
-        if (item.checked) {
-            answer = item.id;
-            // console.log(answer)
-
+    nextButton.addEventListener("click",()=>{
+        count++;
+        console.log(count);
+        console.log(output[count]);
+        if(count>output.length-1){
+            count=output.length-1                       
         }
-
-        // console.log(answer)
+        card.innerHTML=output[count];
+       
     })
-    return answer;
-}
+    
+    prevButton.addEventListener("click",()=>{        
+        count--;
+        console.log(count)
+        if(count<0){
+            count=0            
+        }
+        card.innerHTML=output[count]
+    })
+
+    var answerList = document.querySelectorAll('.answerList')
+    console.log(answerList)
+
+    for(i=0;i<answerList.length;i++){
+        console.log(answerList[i])
+        
+    }
 
 
 
@@ -118,4 +105,12 @@ function getSelect() {
 
 
 
+
+
+
+
+
+
+   
+})
 
